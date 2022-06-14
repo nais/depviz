f66aa5cebefffa377bb87aa7817b4ba1fadedbaa
Truls Jørgensen <trulsjor@gmail.com>
2022-06-14 10:43:18 +0200

    Added hot reload

--- src/hello.clj
+++ src/hello.clj
@@ -9,11 +9,25 @@
   (route/expand-routes
    #{["/greet" :get respond-hello :route-name :greet]}))
 
-(defn create-server []
-  (http/create-server
+(def service-map 
   {::http/routes routes
    ::http/type :jetty
-   ::http/port 8890}))
+   ::http/port 8890})
 
 (defn start []
-  (http/start (create-server)))
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
